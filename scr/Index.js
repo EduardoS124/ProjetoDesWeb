3b0e96a8f074390b097fe9ae647efc5b03a4c773
GelzoNeto <54027463+GelzoNeto@users.noreply.github.com>
2020-02-19 12:04:50 -0300

    Update Index.js

--- scr/Index.js
+++ scr/Index.js
@@ -1,27 +1,35 @@
-const express = require('express')
+const express = require("express")
+const bp = require("body-parser")
 const app = express()
 const port = 8080
 
+app.use(bp.urlencoded())
+
 app.set("view engine", "ejs")
 
-app.get("/", function(req, res) {
-  let op = req.query.op
-  let n1 = parseInt(req.query.n1, 10)
-  let n2 = parseInt(req.query.n2, 10)
-  let resultado = 0
+let funcoes = {
+  soma: (a, b) => a + b,
+  mult: (a, b) => a * b,
+  sub: (a, b) => a - b,
+  div: (a, b) => a / b,
+}
 
-  if (op === "soma"){
-    resultado = n1 + n2
-  }
-  res.render("index", {
-    resultado: resultado
-  })
+app.get("/", function(req, res) {
+  res.render("index")
 })
 
-app.get("/soma/:num1/:num2", function(req, res) {
+app.post("/calc", function(req, res) {
+  let n1 = parseInt(req.body.n1, 10)
+  let n2 = parseInt(req.body.n2, 10)
+  console.log(req.body)
+  let resultado = funcoes[req.body.op](n1, n2);
 
+res.render("resultado", {
+    resultado: resultado
+  })
 })
 
 app.listen(port, function() {
   console.log(`Rodando na porta ${port}!`)
 })
+
